5206be89ec298bb2f17b4e2c882fb558b583dd7b
bayfight <bayu161292@gmail.com>
2018-12-11 14:58:58 +0700

    soal2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state ={
+        setup: '',
+        lucu: ''
+    }
+}
   render() {
+    let klik = () => {
+      let url = `https://safe-falls-22549.herokuapp.com/random_joke`
+      axios.get(url)
+      .then((info) => {
+          this.setState({
+              setup: info.data.setup,
+              lucu: info.data.punchline
+          })
+      })
+  }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="col-lg-12">
+        <div className="row">
+        <div className="container">
+        <div className="col-lg-6 ml">
+        <div id="background">
+            <div className="row">
+            <div id="cardJokes" className=" card bg-white ml mt-5">
+            <h3 className="pt-5 pl-1 pr-3" align="right">{this.state.setup}</h3>
+            <p className="pl-1 pr-3" align="right">{this.state.lucu}</p>
+            <button type="button" class="btn btn-danger pt-2 mr-2" onClick={klik}>Reload  <i class="fas fa-sync-alt"></i></button>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
       </div>
     );
   }
